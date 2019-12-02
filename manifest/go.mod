eeee34559f298991c5c5e2e69c32b7c2ded1fc96
John Jacquay <john@bioteam.net>
2019-12-02 15:00:55 -0700

    Finished manifest generator

---
+++ manifest/go.mod
@@ -0,0 +1,8 @@
+module github.com/jacquayj/gen3-s3indexer-extramural/manifest
+
+go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.25.44
+	github.com/jessevdk/go-flags v1.4.0
+)
--- manifest/main.go
+++ manifest/main.go
@@ -2,8 +2,67 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/jessevdk/go-flags"
 )
 
+var (
+	AWS_ACCESS_KEY_ID     = os.Getenv("AWS_ACCESS_KEY_ID")
+	AWS_SECRET_ACCESS_KEY = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	AWS_REGION            = os.Getenv("AWS_REGION")
+	AWS_BUCKET            = os.Getenv("AWS_BUCKET")
+)
+
+var opts struct {
+	Regexs []string `short:"r" description:"Object keys must match this or be skipped"`
+	Prefix *string  `short:"p" description:"Limits the response to keys that begin with the specified prefix"`
+}
+
+type ParsedRegexes []*regexp.Regexp
+
 func main() {
-	fmt.Print("Hello world")
+
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		panic(err)
+	}
+
+	regexes := make(ParsedRegexes, len(opts.Regexs))
+	for i, rStr := range opts.Regexs {
+		regexes[i] = regexp.MustCompile(rStr)
+	}
+
+	sess := session.Must(session.NewSession())
+	s3Svc := s3.New(sess, &aws.Config{
+		Region:      aws.String(AWS_REGION),
+		Credentials: credentials.NewStaticCredentials(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, ""),
+	})
+
+	bucket := AWS_BUCKET
+	s3Svc.ListObjectsV2Pages(
+		&s3.ListObjectsV2Input{Bucket: &bucket, Prefix: opts.Prefix},
+		func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+			for _, obj := range page.Contents {
+				objKey := *obj.Key
+				if len(regexes) > 0 {
+					for _, exp := range regexes {
+						if exp.Match([]byte(objKey)) {
+							fmt.Println(objKey)
+							break
+						}
+					}
+				} else {
+					fmt.Println(objKey)
+				}
+			}
+			return true
+		},
+	)
+
 }
